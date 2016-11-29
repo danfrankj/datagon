371cf92f5a185c3efdd19a2ead7c6efa94a209bd
Dan Frank <dan.frank@airbnb.com>
2016-11-28 16:56:51 -0800

    add setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='datagon',
+      packages=['datagon'],
+      version='0.0.0',
+      description='An awesome name in need of a project',
+      author='Dan Frank',
+      author_email='danfrankj@gmail.com',
+      url='https://github.com/danfrankj/datagon',
+      download_url='https://github.com/danfrankj/datagon/tarball/0.0.0',
+      keywords=['example'],
+      classifiers=[],
+      )
